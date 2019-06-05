0b517bf8ebce00834312b53b509e5923153e4a8e
AndrewPatskan <stmyni@gmail.com>
2019-06-05 15:15:29 +0100

    firstupdate

--- src/App.js
+++ src/App.js
@@ -8,10 +8,21 @@ class App extends Component {
     this.jump = this.jump.bind(this);
     this.goright = this.goright.bind(this);
     this.goleft = this.goleft.bind(this);
+    this.infinity = this.infinity.bind(this);
     this.state = {
-      pos: 0
+      pos: 0,
+      backpos: 0
     }
   };
+  infinity(){
+    setInterval(move,50);
+    let backk = 0;
+    function move(){
+          backk -=10;
+          const imagge = document.getElementById('road');
+          imagge.style.backgroundPosition = backk + 'px';
+    }
+  }
   jump(){
       const t = setInterval(move,10);
       let pos = 0;
@@ -32,9 +43,14 @@ class App extends Component {
           pos+=10;
           box.style.left = pos + 'px';
           this.setState({pos: this.state.pos + 10});
-          if(pos>=1000){
+          if(pos>=900){
             this.setState({pos: 0});
           }
+          let backk = this.state.backpos;
+          backk -=50;
+          const imagge = document.getElementById('road');
+          imagge.style.backgroundPosition = backk + 'px';
+          this.setState({backpos: this.state.backpos - 50});
   }
 
   goleft(){
@@ -43,20 +59,26 @@ class App extends Component {
       pos-=10;
       box.style.left = pos + 'px';
       this.setState({pos: this.state.pos - 10});
-      if(pos===0){
+      if(pos===10){
         this.setState({pos: 0});
       }
+      let backk = this.state.backpos;
+      backk +=100;
+      const imagge = document.getElementById('road');
+      imagge.style.backgroundPosition = backk + 'px';
+      this.setState({backpos: this.state.backpos + 100});
   }
 
   render() {
     return (
       <div id='container'>
-        <div className="App">
+        <div className="App" id='road'>
             <img src={sonic} className="App-logo" id='son' alt="logo" />
         </div>
         <button id='btn' onClick={this.jump} >jump</button>
         <button id='btn' onClick={this.goright} >right</button>
         <button id='btn' onClick={this.goleft} >left</button>
+        <button id='btn' onClick={this.infinity} >infinity run</button>
       </div>
     );
   }
